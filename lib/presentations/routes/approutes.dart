bbb5d3a91301ad7389bc7e3bbb897485b2a9c721
Yuvraj <yuvilolage7@gmail.com>
2025-07-04 01:34:08 +0530

    initialized app and setup firebase with project

---
+++ lib/presentations/routes/approutes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const home = '/';
+  static const login = '/login';
+  static const signup = '/signup';
+
+}
---
+++ lib/presentations/widgets/bottom navigation bar/bottom_navogation_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavigationBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Color(0xFFCB0033),
+      unselectedItemColor: Color(0xFF414141),
+
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.more_rounded), label: 'More'),
+      ],
+    );
+  }
+}
